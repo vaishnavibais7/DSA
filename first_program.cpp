824a1d3c0dd5f9245cddf9bfb1d9300232172568
vaishnavibais7 <149198095+vaishnavibais7@users.noreply.github.com>
2024-02-25 13:48:24 +0530

    Add files via upload

---
+++ first_program.cpp
@@ -0,0 +1,82 @@
+ /*First Program, Data Type, Variables,Type Casting, Unsigned,Signed, operators*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  /*
+cout <<"NAMASTE logo "<<endl;
+
+*/
+/*Data Type*/
+
+  /*
+int a = 123;
+ cout << a<< endl;
+
+  char b='v';
+  cout<<b<<endl;
+
+  bool ba=true;
+  cout<<ba<<endl;
+
+  float e=2.3;
+  cout<<e<<endl;
+
+  double f=2.34;
+  cout<< f<< endl;
+
+  int size = sizeof(a);
+  cout<<size<<endl;
+*/
+  
+/* Type Casting */
+  
+  /*
+    int a= 'a';
+  cout<<a<<endl;
+
+  char ch=98;
+  cout<<ch<<endl;
+
+  char chh=123456;
+  cout<<chh<<endl;
+*/
+  /* unsigned and signed (how -ve number is stored)*/
+
+  /*
+    unsigned int a = 112;
+  cout<<a<<endl;
+  */
+
+  /*
+  int a =2/5;
+
+  cout<<a<<endl;
+
+  cout<<0.2/5<<endl;
+*/
+
+   /*Operators*/
+  int a=2;
+  int b =3;
+
+  bool first =(a==b);
+  cout<<first<<endl;
+
+  bool second=(a<b);
+  cout<<second<<endl;
+
+  bool third =(a>b);
+  cout<<third<<endl;
+
+  bool fourth =(a>=b);
+  cout<<fourth<<endl;
+
+  bool fifth =(a<=b);
+  cout<<fifth<<endl;
+
+  bool sixth =(a!=b);
+  cout<<sixth<<endl;
+  
+  
+}
