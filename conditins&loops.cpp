c3ecdc818616909e1f272eb090fbcd17606b5c2f
vaishnavibais7 <149198095+vaishnavibais7@users.noreply.github.com>
2024-02-27 22:09:03 +0530

    Add files via upload

---
+++ conditins&loops.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   /*int n;
+  cout<<"Enter the value of n"<<endl;
+  cin>>n;
+
+  cout<<"The value of n is :"<<n<<endl;
+  if(n>0){
+    cout<<"The value of n is positive"<<endl;
+
+  }
+  else{
+    cout<<"The value of n is negative"<<endl;
+  }
+  */
+
+  /*int a,b;
+  cout<<"Enter the value of a"<<endl;
+  cin>>a;
+  cout<<"Enter the value of b"<<endl;
+  cin>>b;
+  if(a>b)
+  {
+    cout<<"A is the greatest number";
+  }
+  if(b>a)
+  {
+    cout<<"B is the greatest number";
+  }
+  else(a=b);
+  {
+      cout<<"Both the numbers are equal";
+  }*/
+
+  //While Loop
+
+  /*int n;
+  cout<<"Enter the number :" ;
+  cin>>n;
+
+  int i= 1;
+
+  while(i<=n)
+    {
+      cout<<"the numbers are:"<<i<<endl;
+
+      i=i+1;
+    }
+  */
+
+  /*int n;
+  cout<<"Enter the number to be added :";
+  cin>>n;
+
+  int i=1;
+  int sum=0;
+
+  while(i<=n)
+    {
+      sum=sum+i;
+      i=i+1;
+    }
+     cout<<"value of the sum is : "<< sum ;
+  */
+
+  //prime number 
+
+  /*int i =2;
+  int n;
+  cout<<"Enter the number to be checked:";
+  cin>>n;
+
+  while(i<n){
+    if(n%i==0){
+      cout<<"The number is not  prime number";
+    }
+    else{
+      cout<<"The number is prime number ";
+    }
+     i=i+1;
+  }
+  */
+ 
+}
